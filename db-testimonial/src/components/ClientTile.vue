2de17edea30f712a2d38a9058a21a718b5d92d8e
Pg Gonni <p.gonni@hotmail.com>
2020-06-30 07:21:38 -0400

    move data out of store
    Realized Vuex may be overkill for such a small project, and would make sense in a real world use anyways

--- db-testimonial/src/components/ClientTile.vue
+++ db-testimonial/src/components/ClientTile.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="bg-gray-400 grid-flow-row flex items-center justify-center">
     <img :src="this.logoURL" alt="Client Logo">
+   <div>Hello</div>
   </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    clients() {
-      return this.$store.state.clients;
-    }
-  }
-};
+
 </script>
