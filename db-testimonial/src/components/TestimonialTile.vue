b01f4078cbc2907d59768c23923997c71ea3f95b
Pg Gonni <p.gonni@hotmail.com>
2020-07-10 18:08:19 -0400

    make testimonial tile padding responsive

--- db-testimonial/src/components/TestimonialTile.vue
+++ db-testimonial/src/components/TestimonialTile.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="absolute w-full h-full p-16 flex flex-col justify-center">
+  <div class="absolute w-full h-full p-8 xl:p-16 flex flex-col justify-center">
     <img :src="logoURL" alt="Client Logo" class="w-logo h-logo object-contain">
-    <div class="w-16 border-b border-gray-400 my-12"></div>
+    <div class="w-16 border-b border-gray-400 my-8"></div>
     <div>"{{ clientQuote }}"</div>
-    <div class="text-sm text-gray-700 mt-12">{{ quoteAuthor }}</div>
+    <div class="text-sm text-gray-700 mt-8">{{ quoteAuthor }}</div>
   </div>
 </template>
 
