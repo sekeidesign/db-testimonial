9ccaeb9c00ece49d46c3bc9c63179cabfc33392c
Pg Gonni <p.gonni@hotmail.com>
2020-07-23 21:30:27 -0400

    failed attempt at implementing timer animation

--- db-testimonial/src/components/OGBadge.vue
+++ db-testimonial/src/components/OGBadge.vue
@@ -1,3 +1,3 @@
 <template>
-  <a href="https://www.dropbox.com/business" class="fixed shadow-md bottom-0 right-0 m-8 px-4 py-2 text-sm bg-gray-200 hover:bg-gray-400">View the Original</a>
+  <a href="https://www.dropbox.com/business" target="_blank" class="fixed shadow-md bottom-0 right-0 m-8 px-4 py-2 text-sm bg-gray-200 hover:bg-gray-400">View the Original</a>
 </template>
--- db-testimonial/src/components/TimerBar.vue
+++ db-testimonial/src/components/TimerBar.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="absolute top-0 left-0 w-full h-bar bg-black origin-left"></div>
+  <transition name="timer">
+    <div class="absolute top-0 left-0 w-full h-bar bg-black origin-left"></div>
+  </transition>
 </template>
+
+<style>
+  .timer-enter-active {
+    transition: all 5s linear;
+  }
+  .timer-leave-active {
+    transition: all 0s linear;
+  }
+  .timer-enter, .timer-leave-to {
+    transform: scaleX(0);
+  }
+  .timer-enter-to, .timer-leave {
+    transform: scaleX(1);
+  }
+</style>
 
 <script>
 export default {
